75f128c52a0a0b8e71dcc2cfc6b121ab7c0d2ef8
cnebreac <91189298+cnebreac@users.noreply.github.com>
2026-01-26 11:38:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1034,7 +1034,7 @@ Entrenamientos de alto enfoque en grupos muy reducidos para maximizar el aprendi
 
         cal = calendar(
             events=events,
-            options={"initialView": "dayGridMonth", "height": 600, "locale": "es", "firstDay": 1},
+            options={"initialView": "dayGridMonth", "height": 600, "locale": "es", "firstDay": 0},
             custom_css=custom_css,
             key="cal",
         )
